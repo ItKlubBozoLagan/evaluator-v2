df6235cd69f5a162c41c2cf7a342bba0a5dcd0d7
Antony1060 <antoniostignjedec@gmail.com>
2024-11-15 12:10:51 +0100

    Fix

--- src/tracing.rs
+++ src/tracing.rs
@@ -4,15 +4,12 @@ use tracing_subscriber::{EnvFilter, FmtSubscriber};
 pub fn setup_tracing() {
     let filter = EnvFilter::new(format!("kontestis_evaluator_v2={}", Level::DEBUG));
 
-    #[cfg(debug_assertions)]
     let subscriber = FmtSubscriber::builder()
-        .with_max_level(Level::DEBUG)
-        .with_env_filter(filter)
-        .finish();
-
-    #[cfg(not(debug_assertions))]
-    let subscriber = FmtSubscriber::builder()
-        .with_max_level(Level::INFO)
+        .with_max_level(if cfg!(debug_assertions) {
+            Level::DEBUG
+        } else {
+            Level::INFO
+        })
         .with_env_filter(filter)
         .finish();
 
