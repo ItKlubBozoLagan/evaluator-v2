e38a7db2eda31465b6bc111384bf44a58a61eba1
Antonio F. Trstenjak <antoniostignjedec@gmail.com>
2024-11-28 01:30:36 +0100

    Very basic integration with dashboard

--- src/evaluate/mod.rs
+++ src/evaluate/mod.rs
@@ -6,7 +6,7 @@ mod runnable;
 mod types;
 
 use crate::evaluate::compilation::CompilationError;
-use crate::messages::Evaluation;
+use crate::messages::{CheckerData, Evaluation, EvaluationLanguage};
 use serde::Serialize;
 
 #[derive(Debug, Serialize)]
@@ -52,10 +52,56 @@ pub enum Verdict {
     Skipped,
 }
 
+// FIXME: don't look under this point, big W.I.P.
+
+#[derive(Debug, Clone, serde::Deserialize, serde::Serialize)]
+pub struct EvaluationDashboard {
+    pub id: u64,
+    pub request_json: String,
+    pub request_json_pretty: String,
+    pub kind: EvaluationKind,
+}
+
+#[derive(Debug, Clone, serde::Deserialize, serde::Serialize)]
+pub enum EvaluationKind {
+    Batch {
+        code: String,
+        language: EvaluationLanguage,
+        time_limit: u32,
+        memory_limit: u32,
+        checker: Option<CheckerData>,
+    },
+    Interactive {
+        code: String,
+        language: EvaluationLanguage,
+        time_limit: u32,
+        memory_limit: u32,
+        checker: CheckerData,
+    },
+    OutputOnly {
+        output: String,
+        checker: Option<CheckerData>,
+    },
+}
+
 pub fn begin_evaluation(
     evaluation: &Evaluation,
     boxes: &[u8],
 ) -> Result<SuccessfulEvaluation, CompilationError> {
+    // FIXME:
+    let client = reqwest::blocking::Client::new();
+
+    // TODO: env
+    // FIXME: unwraps
+    client
+        .post("http://localhost:8888/api/evaluations")
+        .json(&EvaluationDashboard::from((
+            evaluation,
+            serde_json::to_string(&evaluation).unwrap(),
+        )))
+        .send()
+        .unwrap();
+
     match evaluation {
         Evaluation::Batch(batch_evaluation) => types::batch::evaluate(batch_evaluation, boxes[0]),
         Evaluation::OutputOnly(output_only_evaluation) => {
@@ -63,6 +109,37 @@ pub fn begin_evaluation(
         }
         Evaluation::Interactive(interactive_evaluation) => {
             types::interactive::evaluate(interactive_evaluation, boxes[0], boxes[1])
+        }
+    }
+}
+
+impl From<(&Evaluation, String)> for EvaluationDashboard {
+    fn from((value, initial_payload): (&Evaluation, String)) -> Self {
+        Self {
+            id: value.get_evaluation_id(),
+            request_json: initial_payload.to_string(),
+            // FIXME: repetition
+            request_json_pretty: initial_payload,
+            kind: match value.clone() {
+                Evaluation::Batch(batch) => EvaluationKind::Batch {
+                    code: batch.code,
+                    memory_limit: batch.memory_limit,
+                    time_limit: batch.time_limit,
+                    language: batch.language,
+                    checker: batch.checker,
+                },
+                Evaluation::Interactive(interactive) => EvaluationKind::Interactive {
+                    code: interactive.code,
+                    memory_limit: interactive.memory_limit,
+                    time_limit: interactive.time_limit,
+                    language: interactive.language,
+                    checker: interactive.checker,
+                },
+                Evaluation::OutputOnly(output_only) => EvaluationKind::OutputOnly {
+                    output: output_only.output,
+                    checker: output_only.checker,
+                },
+            },
         }
     }
 }
